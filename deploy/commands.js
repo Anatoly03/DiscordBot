7d0fa22a1f84f38bccc8f892ba3cb4cd81917f3c
Anatoly03 <anatoly.wein@gmail.com>
2022-01-14 16:34:27 +0100

    add: slash commands deploy

---
+++ deploy/commands.js
@@ -0,0 +1,43 @@
+import { config } from 'dotenv'
+import { Client, Intents } from 'discord.js'
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v9'
+
+config()
+
+const commands = [
+    new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with pong!'),
+    new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Replies with server info!'),
+    new SlashCommandBuilder()
+        .setName('user')
+        .setDescription('Replies with user info!'),
+].map((command) => command.toJSON())
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS],
+})
+
+client.on('ready', async () => {
+    const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN)
+    const guilds = await client.guilds.fetch()
+
+    guilds.forEach(
+        async (guild) =>
+            await rest.put(
+                Routes.applicationGuildCommands(client.user.id, guild.id),
+                {
+                    body: commands,
+                }
+            )
+    )
+
+    console.log('\x1b[32mSuccessfully registered application commands.\x1b[0m')
+    client.destroy()
+})
+
+client.login(process.env.DISCORD_TOKEN)
