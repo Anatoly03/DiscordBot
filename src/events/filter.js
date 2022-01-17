ef10289b86edc7b4ad7f9bd5e85c4059f53d8c61
Anatoly03 <anatoly.wein@gmail.com>
2022-01-17 16:32:41 +0100

    add: temp: bad-words filter

---
+++ src/events/filter.js
@@ -0,0 +1,15 @@
+import { Message } from 'discord.js'
+import Filter from 'bad-words'
+
+const filter = new Filter()
+
+export const name = 'messageCreate'
+
+/**
+ * @param {Message} message
+ */
+export async function run(message) {
+    if (filter.isProfane(message.content)) {
+        message.delete()
+    }
+}
