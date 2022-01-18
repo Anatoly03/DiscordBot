f64d2be889cdb587a2b78824cc107057cdbd96cb
Anatoly03 <anatoly.wein@gmail.com>
2022-01-18 19:29:31 +0100

    add: text commands

--- src/events/commands.js
+++ src/events/commands.js
@@ -1,9 +1,9 @@
-import { Interaction } from 'discord.js'
+import { Interaction, Message } from 'discord.js'
 
 /**
  * @param {Interaction} interaction
  */
-async function run(interaction) {
+async function interaction_command(interaction) {
     if (!interaction.isCommand()) return
     const command = interaction.client.commands.get(interaction.commandName)
     if (!command) return interaction.reply(':no_entry: Command not found.')
@@ -11,11 +11,26 @@ async function run(interaction) {
 }
 
 /**
+ * @param {Message} message
+ */
+async function message_command(message) {
+    if (message.channel.type === 'DM') return
+    if (!message.content.match(/[\.\-!].+/)) return
+    let input = message.content.substring(1).match(/".*"|`.+`|[a-zA-Z0-9\-\.]+/g)
+    
+    console.log(input)
+}
+
+/**
  * @export
  */
- export default [
+export default [
     {
         name: 'interactionCreate',
-        run,
+        run: interaction_command,
+    },
+    {
+        name: 'messageCreate',
+        run: message_command,
     },
 ]
