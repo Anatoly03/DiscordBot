f561e7d765c1edce2c65b53c2a06571f510d941d
Anatoly03 <anatoly.wein@gmail.com>
2022-01-22 10:59:40 +0100

    add: warn templates

--- src/commands/warn.js
+++ src/commands/warn.js
@@ -4,11 +4,36 @@ import { SlashCommandBuilder } from '@discordjs/builders'
 export const definition = new SlashCommandBuilder()
     .setName('warn')
     .setDescription('Warn a user')
-    .addUserOption((o) =>
-        o.setName('user').setDescription('User to warn').setRequired(true)
+    .addSubcommand((subcommand) =>
+        subcommand
+            .setName('template')
+            .setDescription('Warn User by Template')
+            .addUserOption((option) =>
+                option.setName('user').setDescription('User').setRequired(true)
+            )
+            .addStringOption((option) =>
+                option
+                    .setName('template')
+                    .setDescription('Warning Template')
+                    .setRequired(true)
+            )
     )
-    .addIntegerOption((o) =>
-        o.setName('points').setDescription('Points to take away')
+    .addSubcommand((subcommand) =>
+        subcommand
+            .setName('config')
+            .setDescription('Warn User by Template')
+            .addUserOption((option) =>
+                option.setName('user').setDescription('User').setRequired(true)
+            )
+            .addStringOption((o) =>
+                o
+                    .setName('description')
+                    .setDescription('Warning Description')
+                    .setRequired(true)
+            )
+            .addIntegerOption((o) =>
+                o.setName('points').setDescription('Warning Points')
+            )
     )
 
 /**
@@ -17,7 +42,21 @@ export const definition = new SlashCommandBuilder()
  */
 export function run(client, interaction) {
     let user = interaction.options.getUser('user')
-    let points = interaction.options.getInteger('points') || 0
+    let points = 0
+    let description
+
+    switch (interaction.options.getSubcommand()) {
+        case 'template':
+            ;({ points, description } = get_template(
+                interaction.options.getString('template')
+            ))
+            break
+
+        case 'config':
+            points = interaction.options.getInteger('points')
+            description = interaction.options.getString('description')
+            break
+    }
 
     let embed = new MessageEmbed()
         .setColor(0xf0ad4e)
@@ -28,5 +67,40 @@ export function run(client, interaction) {
                 `:level_slider: **${points}** points`
         )
 
-    interaction.reply({ embeds: [embed] })
+    let dm_embed = new MessageEmbed()
+        .setColor(0xf0ad4e)
+        .setDescription(
+            ':warning: **You have been warned!**\n\n' + description
+        )
+        .setFooter({
+            text: `This warning weights ${points} points.`,
+        })
+
+    interaction.reply({ embeds: [embed, dm_embed] })
+}
+
+/**
+ * @param {Client} client
+ * @param {Interaction} interaction
+ */
+function get_template(s) {
+    switch (s) {
+        case 'nsfw':
+            return {
+                points: 10,
+                description: 'NSFW Content',
+            }
+
+        case 'spam-major':
+            return {
+                points: 5,
+                description: 'Spam',
+            }
+
+        case 'spam':
+            return {
+                points: 1,
+                description: 'Spam (Minor)',
+            }
+    }
 }
